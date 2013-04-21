9fb1c9b29c826ac4ce3cb2f6cec0e39d963c53ae
Will Pragnell <wpragnell@gmail.com>
2013-04-21 16:53:23 +0100

    abort if no placeholder favicon.ico in home dir

--- favme.js
+++ favme.js
@@ -6,8 +6,18 @@ var faviconExists = function() {
   return fs.existsSync("./favicon.ico");
 };
 
+var placeholderFaviconExists = function() {
+  return fs.existsSync(process.env["HOME"] + "/.favicon.ico");
+};
+
 if (faviconExists()) {
   console.log("favicon.ico already present - aborting");
+  process.exit(0);
+}
+
+if (!placeholderFaviconExists()) {
+  console.log("placeholder favicon.ico not found");
+  console.log("please add a .favicon.ico file to your home directory");
   process.exit(0);
 }
 
