1df0104b52795f3762eaa2d1c1090f8cd11d9426
Will Pragnell <wpragnell@gmail.com>
2013-04-21 16:44:57 +0100

    abort if favicon.ico already present

--- favme.js
+++ favme.js
@@ -1,3 +1,13 @@
 #!/usr/bin/env node
 
-console.log("favme fam");
+var fs = require('fs');
+
+var faviconExists = function() {
+  return fs.existsSync("./favicon.ico");
+};
+
+if (faviconExists()) {
+  console.log("favicon.ico already present - aborting");
+  process.exit(0);
+}
+
